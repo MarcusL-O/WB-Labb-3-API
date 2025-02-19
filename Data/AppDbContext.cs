cbd5474af6aaae491389cc516e789be0f8104c40
MarcusLO00 <marcus.lisolivebring@iths.se>
2025-02-19 12:39:17 +0100

    initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Labb3_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Labb3_API.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Technology> Technologies { get; set; }
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb3_API.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string TechnologiesUsed { get; set; }
+    }
+}
